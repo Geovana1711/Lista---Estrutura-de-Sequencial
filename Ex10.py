e28b9b2cd358ae06e8d660f6f4b3924445c455c7
Gege <93001221+Geovana1711@users.noreply.github.com>
2026-03-14 19:33:41 -0300

    Add temperature conversion from Celsius to Fahrenheit

---
+++ Ex10.py
@@ -0,0 +1,4 @@
+#Troca de medidas - Fahrenheit e Celsius
+celsius = float(input("Diga a celsius e irei converter para Fahrenheit "))
+convercao = ((celsius * 9)/5) + 32
+print(f"O valor é de {convercao}")
